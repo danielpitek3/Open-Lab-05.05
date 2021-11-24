8efa7837e5b01fa82806b61a0229b13b30c5cf93
Daniel Pitek <Daniel813245@outlook.sk>
2021-11-24 21:03:35 +0100

    done

--- Open-Lab-05.05/StringTools.cs
+++ Open-Lab-05.05/StringTools.cs
@@ -7,6 +7,26 @@ namespace Open_Lab_05._05
         public string AlternatingCaps(string original)
         {
             throw new NotImplementedException();
+            char[] characters = original.ToCharArray();
+            bool Lower = false;
+
+            for (int i = 0; i <= characters.Length - 1; i++)
+            {
+                if (characters[i].ToString() != " ")
+                {
+                    if (Lower == false)
+                    {
+                        characters[i] = characters[i].ToString().ToUpper().ToCharArray()[0];
+                        Lower = true;
+                    }
+                    else
+                    {
+                        characters[i] = characters[i].ToString().ToLower().ToCharArray()[0];
+                        Lower = false;
+                    }
+                }
+            }
+            return new string(characters);
         }
     }
 }
